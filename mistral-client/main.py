09c928eb5547a07c7b21eb20aaee12188330f177
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-03-19 00:34:36 +0100

    First Mistral connector

---
+++ mistral-client/main.py
@@ -0,0 +1,7 @@
+from services.mistral_connector import mistral_chat_completion
+
+prompt = 'Why do some bots try to steal the API keys from my GitHub account repositories?'
+
+mistral_chat_completion(prompt)
+
+
---
+++ mistral-client/services/mistral_connector.py
@@ -0,0 +1,31 @@
+import os
+
+from mistralai import Mistral
+from mistralai.models import SDKError
+
+
+
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
+def mistral_chat_completion(prompt):
+    
+    model = "mistral-large-latest"
+    
+    client=Mistral(api_key=MISTRAL_API_KEY)
+    
+    try:
+        chat_completion = client.chat.complete(
+            model= model,
+            messages = [
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ]
+        )
+        print(chat_completion.choices[0].message.content)
+        return chat_completion.choices[0].message.content
+    
+    except SDKError as err:
+        print(f"Status Code: {err.status_code}")
+        print(f"Error: {err.body}")
